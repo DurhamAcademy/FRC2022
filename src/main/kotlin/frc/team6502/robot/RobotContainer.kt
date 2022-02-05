c08c97ebbcde39303b0ce9006c7f497b36f6f0a4
TateStaples <56731352+TateStaples@users.noreply.github.com>
2022-02-05 11:30:24 -0500

    LEDs

--- src/main/kotlin/frc/team6502/robot/RobotContainer.kt
+++ src/main/kotlin/frc/team6502/robot/RobotContainer.kt
@@ -1,14 +1,22 @@
 package frc.team6502.robot
 
 import edu.wpi.first.wpilibj.DigitalInput
-import frc.team6502.robot.subsystems.Climber
-import frc.team6502.robot.subsystems.Drivetrain
-import frc.team6502.robot.subsystems.Intaker
-import frc.team6502.robot.subsystems.Shooter
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.RobotBase
+import frc.team6502.robot.commands.Intake
+import frc.team6502.robot.commands.climb.Climb
+import frc.team6502.robot.commands.climb.ToggleArmLift
+import frc.team6502.robot.commands.shooter.Flush
+import frc.team6502.robot.subsystems.*
 import kyberlib.auto.Navigator
+import kyberlib.command.Game
 import kyberlib.input.controller.KXboxController
+import kyberlib.lighting.KLEDRegion
+import kyberlib.lighting.KLEDStrip
+import kyberlib.lighting.animations.*
 import kyberlib.sensors.gyros.KPigeon
 import org.photonvision.PhotonCamera
+import java.awt.Color
 import kotlin.math.PI
 
 /**
@@ -34,17 +42,94 @@ object RobotContainer {
             expo = 20.0
             deadband = 0.2
         }
+
+//        leftTrigger.activateAt(0.5).whileActiveOnce(Intake)
+//        leftBumper.whileActiveOnce(Flush)
+//
+//        aButton.toggleWhenActive(Climb)
+//        bButton.whileActiveOnce(ToggleArmLift())
     }
 
+    val navigation = Navigator(gyro, Constants.START_POSE)
 
-    val navigation = Navigator(gyro)
+    val leds = KLEDStrip(0, 103).apply {
+        val length = 103
+        val coral = Color(255, 93, 115)
 
+        this += KLEDRegion(AnimationRGBWave(0.5, 2, false), 0, length)  // default
+
+        // climb
+        val climbColor = Color(255, 255, 0)
+        val climbStart = 0
+        val climbLength = 100
+        this += KLEDRegion(AnimationSolid(climbColor), climbStart, climbLength)
+            {Climber.status == CLIMBER_STATUS.IDLE}
+        this += KLEDRegion(AnimationBlink(climbColor, 5), climbStart, climbLength)
+            {Climber.status == CLIMBER_STATUS.ACTIVE}
+        this += KLEDRegion(AnimationRain(climbColor, 5, 2, false), climbStart, climbLength)
+            {Climber.status == CLIMBER_STATUS.FALLING}
+        this += KLEDRegion(AnimationRain(climbColor, 5, 2, true), climbStart, climbLength)
+            {Climber.status == CLIMBER_STATUS.RISING}
+
+        // conveyor
+        val allianceColor = when(Game.alliance){
+            DriverStation.Alliance.Blue -> Color.BLUE
+            DriverStation.Alliance.Red -> Color.RED
+            DriverStation.Alliance.Invalid -> Color.BLUE
+            null -> Color.BLUE
+        }
+        val enemyColor = when(Game.alliance){
+            DriverStation.Alliance.Blue -> Color.RED
+            DriverStation.Alliance.Red -> Color.BLUE
+            DriverStation.Alliance.Invalid -> Color.RED
+            null -> Color.RED
+        }
+        val conveyorStart = 0
+        val conveyorLength = 50
+        this += KLEDRegion(AnimationSolid(coral), conveyorStart, conveyorLength)
+            {Conveyor.status == CONVEYOR_STATUS.EMPTY}
+        this += KLEDRegion(AnimationBlink(allianceColor, 5), conveyorStart, conveyorLength)
+            {Conveyor.status == CONVEYOR_STATUS.SINGLE_GOOD}
+        this += KLEDRegion(AnimationSolid(allianceColor), conveyorStart, conveyorLength)
+            {Conveyor.status == CONVEYOR_STATUS.FULL_GOOD}
+        this += KLEDRegion(AnimationSolid(enemyColor), conveyorStart, conveyorLength)
+            {Conveyor.status == CONVEYOR_STATUS.BAD}
+        this += KLEDRegion(AnimationPulse(allianceColor, 2), conveyorStart, conveyorLength)
+            {Conveyor.status == CONVEYOR_STATUS.FEEDING}
+
+        // intake
+        val intakeStart = 0
+        val intakeLength = 100
+        this += KLEDRegion(AnimationSolid(coral), intakeStart, intakeLength) {Intaker.deployed}
+
+        // Shooter & Turret
+        val shooterStart = 0
+        val shooterLength = 100
+        this += KLEDRegion(AnimationBlink(Color(255, 0, 0), 5), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.LOST}
+        this += KLEDRegion(AnimationBlink(Color(255, 120, 0), 5), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.NOT_FOUND}
+        this += KLEDRegion(AnimationSolid(Color(255, 255, 0)), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.ADJUSTING}
+        this += KLEDRegion(AnimationBlink(Color(0, 255, 0), 5), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.LOCKED && Shooter.status != SHOOTER_STATUS.HIGH_READY}
+        this += KLEDRegion(AnimationSolid(Color(0, 255, 0)), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.LOCKED && Shooter.status == SHOOTER_STATUS.HIGH_READY}
+        this += KLEDRegion(AnimationPulse(Color(0, 255, 0), 5), shooterStart, shooterLength)
+            {Turret.status == TURRET_STATUS.LOCKED && Shooter.status == SHOOTER_STATUS.AUTO_SHOT}
+        this += KLEDRegion(AnimationPulse(Color(255, 120, 0), 5), shooterStart, shooterLength)
+            {Shooter.status == SHOOTER_STATUS.FORCE_SHOT}
+
+    }
     init {
         // initialize subsystems here:
-        Drivetrain
-        Shooter
         Climber
+        Conveyor
+        Drivetrain
         Intaker
+        Shooter
+        Turret
+
     }
 
 }
