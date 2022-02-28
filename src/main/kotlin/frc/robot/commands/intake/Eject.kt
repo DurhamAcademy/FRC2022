06edff2dea297705efda7b6021f7f7fe3e087ca7
a1cd <71281043+a1cd@users.noreply.github.com>
2022-02-28 18:22:12 -0500

    I think it will work now. Tate, look at it for me and just make sure there are no glaring problems. :)

--- src/main/kotlin/frc/robot/commands/intake/Eject.kt
+++ src/main/kotlin/frc/robot/commands/intake/Eject.kt
@@ -2,6 +2,7 @@ package frc.robot.commands.intake
 
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.kyberlib.math.units.extensions.rotationsPerSecond
+import frc.kyberlib.math.units.extensions.rpm
 import frc.robot.subsystems.Conveyor
 import frc.robot.subsystems.Shooter
 import frc.robot.subsystems.Turret
@@ -16,11 +17,12 @@ object Eject : CommandBase() {
     }
 
     override fun execute() {
-        
-
+        Conveyor.feeder.velocity = 5.rotationsPerSecond
+        Conveyor.indexer.velocity = 10.rotationsPerSecond
     }
 
     override fun end(interrupted: Boolean) {
-
+        Conveyor.feeder.velocity = 5.rpm
+        Conveyor.indexer.velocity = 3.rpm
     }
 }
--- src/main/kotlin/frc/robot/commands/intake/LoadEject.kt
+++ src/main/kotlin/frc/robot/commands/intake/LoadEject.kt
@@ -4,26 +4,23 @@ import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.kyberlib.math.units.extensions.rotationsPerSecond
 import frc.robot.subsystems.Conveyor
 import frc.robot.subsystems.Shooter
-import frc.robot.subsystems.Turret
 
 object LoadEject : CommandBase() {
     override fun initialize() {
         addRequirements(Shooter)
-        addRequirements(Turret)
         addRequirements(Conveyor)
     }
+
     override fun execute() {
         if (Shooter.flywheelMaster.velocity <= 7.rotationsPerSecond)
             Shooter.flywheelMaster.velocity = 7.rotationsPerSecond
-        Turret.turret.velocity = 1.rotationsPerSecond
+        Conveyor.feeder.velocity = 1.rotationsPerSecond
         Conveyor.ConveyorMotor.velocity = 1.rotationsPerSecond
     }
 
     override fun isFinished(): Boolean {
         return (Shooter.flywheelMaster.velocity <= 6.5.rotationsPerSecond) &&
                 (Conveyor.ConveyorMotor.velocity >= 0.25.rotationsPerSecond) &&
-                Turret.turret.velocity < 1.25.rotationsPerSecond
-
+                Conveyor.feeder.velocity < 1.25.rotationsPerSecond
     }
-
 }
