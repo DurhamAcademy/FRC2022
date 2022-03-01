bbbf7e65e04810588e22573d7e6e20ad643857f5
TateStaples <56731352+TateStaples@users.noreply.github.com>
2022-02-28 23:42:18 -0500

    math

--- src/main/kotlin/frc/kyberlib/math/Polar.kt
+++ src/main/kotlin/frc/kyberlib/math/Polar.kt
@@ -3,30 +3,49 @@ package frc.kyberlib.math
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
+import frc.kyberlib.command.Debug
 import frc.kyberlib.math.units.extensions.*
 import frc.kyberlib.math.units.towards
 
-data class PolarPosition(val r: Length, val theta: Angle) {
+data class PolarPosition(val r: Length, val theta: Angle) : Debug {
     fun cartesian(origin: Translation2d): Translation2d = Translation2d(theta.cos * r.meters, theta.sin * r.meters)
-}
-
-data class PolarPose(val r: Length, val theta: Angle, val orientation: Angle) {
-    constructor(position: PolarPosition, orientation: Angle) : this(position.r, position.theta, orientation)
-    fun cartesian(origin: Translation2d): Pose2d {
-        val translation2d = Translation2d(origin.x + theta.cos * r.meters, origin.y + theta.sin * r.meters)
-        return Pose2d(translation2d, orientation.plus(origin.towards(translation2d)))
+    override fun debugValues(): Map<String, Any?> {
+        return mapOf(
+            "r" to r, "theta" to theta
+        )
     }
 }
 
-data class PolarVelocity(val dr: LinearVelocity, val dTheta: AngularVelocity, val dOrientation: AngularVelocity)
+data class PolarPose(val r: Length, val theta: Angle, val orientation: Angle) : Debug {
+    constructor(position: PolarPosition, orientation: Angle) : this(position.r, position.theta, orientation)
+    fun cartesian(origin: Translation2d): Pose2d {
+        val translation2d = Translation2d(origin.x + theta.cos * r.meters, origin.y + theta.sin * r.meters)
+        return Pose2d(translation2d, origin.towards(translation2d).minus(orientation))
+    }
+
+    override fun debugValues(): Map<String, Any?> {
+        return mapOf(
+            "r" to r, "theta" to theta, "orientation" to orientation
+        )
+    }
+}
+
+data class PolarVelocity(val dr: LinearVelocity, val dTheta: AngularVelocity, val dOrientation: AngularVelocity) : Debug {
+    override fun debugValues(): Map<String, Any?> {
+        return mapOf(
+            "∆r" to dr, "∆theta" to dTheta, "∆orientation" to dOrientation
+        )
+    }
+
+}
 
 fun Translation2d.polar(origin: Translation2d): PolarPosition = PolarPosition(
     getDistance(origin).meters,
     origin.towards(this).k
 )
 fun Pose2d.polar(origin: Translation2d): PolarPose = PolarPose(
-    this.translation.polar(origin),
-    origin.towards(this.translation).minus(this.rotation).k
+    translation.polar(origin),
+    rotation.minus(translation.towards(origin)).k
 )
 fun ChassisSpeeds.polar(position: PolarPose): PolarVelocity {
     val forward = vxMetersPerSecond.metersPerSecond
@@ -35,7 +54,7 @@ fun ChassisSpeeds.polar(position: PolarPose): PolarVelocity {
     val sin = position.orientation.sin
     return PolarVelocity(
         forward * cos + strafe * sin,
-        (forward * sin + strafe * cos) / position.r,
+        -(forward * sin + strafe * cos) / position.r,
         omegaRadiansPerSecond.radiansPerSecond
     )
 }
