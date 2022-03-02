b46c83ab09ceb1127c63eb4431e2b6f8b128a613
TateStaples <56731352+TateStaples@users.noreply.github.com>
2022-03-01 22:54:15 -0500

    Update Eject.kt
    condense requirements and add shooter updates

--- src/main/kotlin/frc/robot/commands/intake/Eject.kt
+++ src/main/kotlin/frc/robot/commands/intake/Eject.kt
@@ -7,18 +7,17 @@ import frc.robot.subsystems.Shooter
 object Eject : CommandBase() {
     /// TODO: 2/25/22 add Eject Code
     override fun initialize() {
-        addRequirements(Shooter)
-        addRequirements(Conveyor)
+        addRequirements(Shooter, Conveyor)
         beforeStarting(LoadEject)
     }
 
     override fun execute() {
+        Shooter.updateVoltage()
         Conveyor.feeder.voltage = 0.9
         Conveyor.indexer.voltage = 0.35
     }
 
     override fun end(interrupted: Boolean) {
-        Conveyor.feeder.voltage = 0.0
-        Conveyor.indexer.voltage = 0.1
+        Shooter.stop()
     }
 }
