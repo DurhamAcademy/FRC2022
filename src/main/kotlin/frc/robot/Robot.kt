d8a34a1bdeeb0a3c908253747ce27143949dfbf0
TateStaples <56731352+TateStaples@users.noreply.github.com>
2022-04-23 12:36:29 -0400

    new branch

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -1,83 +1,21 @@
 package frc.robot
 
 import edu.wpi.first.math.geometry.Pose2d
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
-import frc.kyberlib.auto.trajectory.TrajectoryManager
-import frc.kyberlib.command.Game
 import frc.kyberlib.command.KRobot
-import frc.kyberlib.math.units.extensions.degrees
-import frc.kyberlib.math.units.extensions.meters
-import frc.kyberlib.math.units.string
-import frc.kyberlib.math.units.zeroPose
-import frc.kyberlib.simulation.field.KField2d
-import frc.robot.commands.shooter.Shoot
-import frc.robot.subsystems.*
-import java.io.File
+import frc.robot.subsystems.Drivetrain
 
 class Robot : KRobot() {
-    private var autoCommand: Command? = null
     override fun robotInit() {
         RobotContainer  // initializes object
-
-        Climber.leftWinch.resetPosition(0.degrees)
-        Climber.rightWinch.resetPosition(0.degrees)
     }
 
-    override fun robotPeriodic() {
-        if (Game.real) {
-            // log relevant stuff
-            SmartDashboard.putNumber("gyro", RobotContainer.gyro.heading.degrees)
-            SmartDashboard.putBoolean("visible", Limelight.targetVisible)
-            SmartDashboard.putBoolean("shooter ready", Shooter.ready)
-            SmartDashboard.putNumber("distance", Limelight.distance.meters)
-            SmartDashboard.putString("pose", (RobotContainer.navigation.position - Constants.HUB_POSITION).string)
-        }
-        KField2d["turret"].pose = RobotContainer.navigation.pose
-    }
 
     override fun disabledInit() {
         Drivetrain.stop()
     }
 
-    override fun autonomousInit() {
-        val auto = loadRoutine(RobotContainer.autoChooser.selected)//RobotContainer.autoChooser.selected!!)
-        auto.schedule()
-        autoCommand = auto
-    }
-
     private fun reset(pose: Pose2d) {
         // reset navigation to a specific pose
         RobotContainer.navigation.pose = pose
-    }
-
-    private fun loadRoutine(routine: String): SequentialCommandGroup {
-        val command = SequentialCommandGroup()
-        TrajectoryManager.routines
-        val f = File("${TrajectoryManager.AUTO_PATH}/$routine")
-        f.readLines().forEach {
-//            when (it) {
-//                "Shot" -> command.addCommands(Shoot())
-//                else -> command.addCommands(AutoDrive(TrajectoryManager[it]!!))
-//            }
-        }
-
-        // resets our position assuming we are in the right spot
-        var pose = zeroPose
-        for (it in f.readLines()) {
-            if (it != "Shot") pose = TrajectoryManager[it]!!.initialPose
-            break
-        }
-
-        reset(pose)
-        return command
-    }
-
-    override fun autonomousExit() {
-        KField2d.trajectory = null
-        autoCommand?.cancel()
-        Intaker.deployed = false
-        Intaker.intakeMotor.stop()
     }
 }
--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,18 +1,12 @@
 package frc.robot
 
-import edu.wpi.first.math.filter.Debouncer
-import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import frc.kyberlib.auto.Navigator
 import frc.kyberlib.auto.trajectory.TrajectoryManager
 import frc.kyberlib.input.controller.KXboxController
 import frc.kyberlib.sensors.gyros.KNavX
-import frc.robot.commands.climb.Climb
-import frc.robot.commands.intake.Intake
-import frc.robot.commands.shooter.ForceShoot
-import frc.robot.commands.shooter.Shoot
-import frc.robot.subsystems.*
+import frc.robot.subsystems.Drivetrain
 
 /**
  * Contains all Robot subsystems and sensors
@@ -20,20 +14,11 @@ import frc.robot.subsystems.*
 object RobotContainer {
     // initialize sensors and inputs here
     val gyro = KNavX()
-    val limelight = frc.kyberlib.sensors.Limelight()
-
-    // hall sensor
-    val turretLimit = DigitalInput(0)
 
     val navigation = Navigator(gyro, Constants.START_POSE)
 
     // controllers
-    val controller = KXboxController(0).apply {
-        aButton.debounce(.3, Debouncer.DebounceType.kFalling).whileActiveOnce(Intake)
-        rightTrigger.activateAt(.5).whenHeld(Shoot())
-        rightBumper.whenHeld(ForceShoot)
-        upDPad.toggleWhenPressed(Climb)
-    }
+    val controller = KXboxController(0).apply {}
 
     // auto path chooser
     val autoChooser = SendableChooser<String>().apply {
@@ -45,12 +30,7 @@ object RobotContainer {
 
     init {
         // initialize subsystems here:
-        Climber
-        Conveyor
         Drivetrain
-        Intaker
-        Shooter
-        Limelight
     }
 
 }
